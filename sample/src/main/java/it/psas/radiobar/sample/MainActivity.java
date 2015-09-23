ecb96d00629cc24de2ff174b51e50effdefa9f5a
Alessandro Contenti <a.contenti@gmail.com>
2015-09-23 23:41:21 +0200

    bug fixes in sample and code cleanup

--- sample/src/main/java/it/psas/radiobar/sample/MainActivity.java
+++ sample/src/main/java/it/psas/radiobar/sample/MainActivity.java
@@ -1,21 +1,20 @@
-package it.psas.radiobarsample;
+package it.psas.radiobar.sample;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import it.psas.radiobar.RadioBar;
 import it.psas.radiobar.RadioImageView;
+import it.psas.radiobarsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
-	int mSelectedRadioImageViewId = 0;
-	int mSelectedRadioImageViewOldId = 0;
+	int mSelectedRadioImageViewPosition = 0;
+	int mSelectedRadioImageViewOldPosition = 0;
 	private CoordinatorLayout mCoordinatorLayout;
 	private RadioBar mRadioBar;
 
@@ -30,43 +29,23 @@ public class MainActivity extends AppCompatActivity {
 		mRadioBar.setOnSelectedItemChangeListener(new RadioBar.OnSelectedItemChangeListener() {
 			@Override
 			public void onSelectedItemChange(RadioBar radioBar, int id, int position) {
-				mSelectedRadioImageViewId = id;
+				mSelectedRadioImageViewOldPosition = mSelectedRadioImageViewPosition;
+				mSelectedRadioImageViewPosition = position;
 				RadioImageView mSelectedRadioImageView = (RadioImageView) findViewById(id);
 				Snackbar.make(mCoordinatorLayout, "Selected item at position " + position + "!", Snackbar.LENGTH_LONG)
 						.setAction("UNDO", mSnackBarActionClickListener)
 						.show();
 			}
 		});
-		mSelectedRadioImageViewId = mRadioBar.getSelectedRadioImageViewId();
+		mRadioBar.checkAtPosition(0);
+		int mSelectedRadioImageViewId = mRadioBar.getSelectedRadioImageViewId();
 		RadioImageView mSelectedRadioImageView = (RadioImageView) findViewById(mSelectedRadioImageViewId);
 	}
 
 	View.OnClickListener mSnackBarActionClickListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			mRadioBar.check(mSelectedRadioImageViewOldId);
-			mSelectedRadioImageViewOldId = mSelectedRadioImageViewId;
+			mRadioBar.checkAtPosition(mSelectedRadioImageViewOldPosition);
 		}
 	};
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
